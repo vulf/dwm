491601b36d2eb16e619794b422f0de9d85656979
vulf <pranavsivvam@gmail.com>
2020-07-16 21:16:59 +0530

    minor change in rules

--- config.def.h
+++ config.def.h
@@ -46,7 +46,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "burp-StartBurp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       0,       0,           -1 },
 };
 
 /* layout(s) */
