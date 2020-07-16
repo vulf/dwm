256eb009b156acb8eace7b73bf154c11ba1759a8
vulf <pranavsivvam@gmail.com>
2020-07-16 20:18:29 +0530

    modified colours

--- config.def.h
+++ config.def.h
@@ -12,13 +12,15 @@ static const char col_gray2[]       = "#F3F6F9"; // white
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_gray5[]       = "#005577"; // default dwm blue
+static const char col_fg[]         = "#99e7ff";
+static const char col_bar[]         = "#1b272c";
 static const char col_cyan[]        = "#439DB6"; // light blue
 static const unsigned int baralpha = 50;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][4]      = {
 	/*               fg         bg         border     float */
-	[SchemeNorm] = { col_gray3, col_gray1, col_cyan, col_gray4 },
-	[SchemeSel] =  { col_gray4, col_cyan,  col_gray2, col_cyan },
+	[SchemeNorm] = { col_cyan, col_bar, col_cyan, col_gray4 },
+	[SchemeSel] =  { col_fg, col_bar,  col_gray2, col_cyan },
 };
 /* Custom */
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
@@ -72,7 +74,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bar, "-nf", col_cyan, "-sb", col_bar, "-sf", col_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
