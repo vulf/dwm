c1203ac975337628b1fabdc24b1f241a43a20276
vulf <pranavsivvam@gmail.com>
2020-07-18 12:11:53 +0530

    changed FontAwesome size

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack:size=10",
-                                        "FontAwesome:Regular:size=10"
+                                        "FontAwesome:Regular:size=11"
 };
 static const char dmenufont[]       = "Hack:size=10";
 static const char col_gray0[]       = "#000000";
