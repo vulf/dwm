8099a5604f482a71ccb6578f874a4d3f1cfb5194
vulf <pranavsivvam@gmail.com>
2020-08-21 12:58:54 +0530

    modified tag icons

--- config.def.h
+++ config.def.h
@@ -42,7 +42,7 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 // static const char *tags[] = { "", "", "", "", "", "6", "7", "8", "9" };
-static const char *tags[] = { "1:", "2:", "3:", "4:", "5:", "6:", "7:", "8:" };
+static const char *tags[] = { "1:", "2:", "3:", "4:", "5:", "6:", "7:", "8:" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -59,7 +59,8 @@ static const Rule rules[] = {
 	{ "Cherrytree",     NULL,      NULL,       1 << 4,            0,           -1 },
 	{ "Evince",     NULL,      NULL,           1 << 4,                    0,           -1 },
 	{ "discord",     NULL,      NULL,           1 << 5,                    0,           -1 },
-	{ "SimpleScreenRecorder",  NULL,   NULL,       1 << 6,            0,           -1 }
+	{ "SimpleScreenRecorder",  NULL,   NULL,       1 << 6,            0,           -1 },
+	{ "Thunar",  NULL,   NULL,       1 << 7,            0,           -1 }
 };
 
 /* layout(s) */
