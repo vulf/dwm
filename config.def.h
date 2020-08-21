9a79f2b6044a9a53bf7ca28f7fb6a60ac988f846
vulf <pranavsivvam@gmail.com>
2020-08-21 12:38:31 +0530

    tag rules - minor changes

--- config.def.h
+++ config.def.h
@@ -42,7 +42,7 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 // static const char *tags[] = { "", "", "", "", "", "6", "7", "8", "9" };
-static const char *tags[] = { "1:", "2:", "3:", "4:", "5:", "6:", "7:" };
+static const char *tags[] = { "1:", "2:", "3:", "4:", "5:", "6:", "7:", "8:" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -57,6 +57,7 @@ static const Rule rules[] = {
 	{ "VirtualBox Manager", NULL,      NULL,  1 << 3,            0,           -1 },
 	{ "VirtualBox Machine", NULL,      NULL,  1 << 3,            0,           -1 },
 	{ "Cherrytree",     NULL,      NULL,       1 << 4,            0,           -1 },
+	{ "Evince",     NULL,      NULL,           1 << 4,                    0,           -1 },
 	{ "discord",     NULL,      NULL,           1 << 5,                    0,           -1 },
 	{ "SimpleScreenRecorder",  NULL,   NULL,       1 << 6,            0,           -1 }
 };
